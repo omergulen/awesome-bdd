5195c5d6c269be928c3468e79233532b558c60bc
Cemal Kılıç <cemalkilic96@gmail.com>
2021-01-18 20:33:32 +0300

    🧼 Clean up Layout
    Co-Authored-By: Omer Gulen <omrglen@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </React.Fragment>
 )
 
 export default NotFoundPage
