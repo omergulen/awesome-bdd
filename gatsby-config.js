eab244cbb85174c35bf8beadfb92066e34071ad9
omergulen <omergulen@std.iyte.edu.tr>
2021-01-18 13:46:22 +0300

    Add, theme-ui & tsx support
    Co-Authored-By: Cemal Kilic <cemalkilic96@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
