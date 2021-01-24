5bace4dd172652b8a2fc79dd84a0908b0f1d3be5
omergulen <omergulen@std.iyte.edu.tr>
2021-01-24 15:19:44 +0300

    🧼 Index :: Clean-ups

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,13 +78,12 @@ const Index = ({
         <meta name="description" content={siteDescription} />
         <meta name="image" content={`${siteUrl}/social.png`} />
         <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content="lekoarts.de" />
+        <meta property="og:site_name" content="bdd.tools" />
         <meta property="og:url" content={siteUrl} />
         <meta property="og:title" content={siteName} />
         <meta property="og:description" content={siteDescription} />
         <meta property="og:image" content={`${siteUrl}/social.png`} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@lekoarts_de" />
         <meta name="twitter:title" content={siteName} />
         <meta name="twitter:description" content={siteDescription} />
         <meta name="twitter:image" content={`${siteUrl}/social.png`} />
@@ -101,17 +100,6 @@ const Index = ({
         })}
       >
         Copyright &copy; {new Date().getFullYear()}. All rights reserved.
-        <br />
-        <div css={css({ display: `flex`, justifyContent: `center`, alignItems: `center`, marginTop: `1rem` })}>
-          <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_w30.png" alt="LekoArts Logo" />
-          {` `}
-          <a
-            css={css({ color: `#525f74`, marginLeft: `1rem`, textDecoration: `none` })}
-            href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-status-dashboard"
-          >
-            Theme by LekoArts
-          </a>
-        </div>
       </footer>
     </main>
   )
