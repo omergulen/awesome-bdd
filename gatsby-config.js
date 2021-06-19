4933b851ba465f1222cf8f5a19f8f3a5db357114
Omer Gulen <omergulen@jotform.com>
2021-06-19 13:10:32 +0300

    SiteMetadata :: Update for awesome-bdd

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    siteDescription: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteName: `@gatsbyjs`,
-    siteUrl: `https://github.com`,
+    title: `Awesome BDD`,
+    siteDescription: `Everything about Behavior-driven Development. BDD tools, libraries, tutorials, videos, aritcles and more.`,
+    description: `Everything about Behavior-driven Development. BDD tools, libraries, tutorials, videos, aritcles and more.`,
+    author: `@omergulen`,
+    siteName: `Awesome BDD`,
+    siteUrl: `https://bdd.tools`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -47,7 +47,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/awesome.png`, // This path is relative to the root of the site.
       },
     },
     {
